0936ee5f7e1392aa7603381228c0e154b5ca2ed0
kadirermantr <kadirermantr@gmail.com>
2024-06-19 22:10:03 +0300

    change home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,15 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class LuckyController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/lucky', name: 'app_lucky')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $number = random_int(0, 100);
-
-        return $this->render('lucky/index.html.twig', [
-            'number' => $number,
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
